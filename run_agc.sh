578c2033c942f2c733a3454a11b84633831b85af
pfackeldey <fackeldey.peter@gmail.com>
2026-05-08 19:00:06 +0200

    uncomment again

--- run_agc.sh
+++ run_agc.sh
@@ -2,24 +2,24 @@
 
 action() {
   # --- 1. Modern coffea 2024+ (buffer-cache based) ---
-#   for cache in "inmemory" "inmemory_lru_100MiB" "inmemory_lru_500MiB" "ondisk"
-#   do
-#     for codec in "none" "blosc"
-#     do
-#       for ((entry_stop=100000; entry_stop<=1000000; entry_stop+=50000))
-#       do
-#         echo "Running: pixi run python benchmarks/agc.py $cache $codec $entry_stop"
-#         pixi run python benchmarks/agc.py $cache $codec $entry_stop
-#       done
-#     done
-#   done
+  for cache in "inmemory" "inmemory_lru_100MiB" "inmemory_lru_500MiB" "ondisk"
+  do
+    for codec in "none" "blosc"
+    do
+      for ((entry_stop=100000; entry_stop<=1000000; entry_stop+=50000))
+      do
+        echo "Running: pixi run python benchmarks/agc.py $cache $codec $entry_stop"
+        pixi run python benchmarks/agc.py $cache $codec $entry_stop
+      done
+    done
+  done
 
-  # --- 2. Modern coffea vanilla ---
-#   for ((entry_stop=100000; entry_stop<=1000000; entry_stop+=50000))
-#   do
-#     echo "Running: pixi run python benchmarks/agc.py nocache none $entry_stop"
-#     pixi run python benchmarks/agc.py nocache none "$entry_stop"
-#   done
+  --- 2. Modern coffea vanilla ---
+  for ((entry_stop=100000; entry_stop<=1000000; entry_stop+=50000))
+  do
+    echo "Running: pixi run python benchmarks/agc.py nocache none $entry_stop"
+    pixi run python benchmarks/agc.py nocache none "$entry_stop"
+  done
 
   # --- 2. Legacy coffea 0.7.x (no buffer cache support) ---
   cd benchmarks/agc07
